06d3cafc7053c55b978fac4f2dcd5420a5db14fa
Akilesh-programmer <akileshakileshs1234@gmail.com>
2021-11-21 17:20:54 +0530

    pasted the code

---
+++ functions.py
@@ -0,0 +1,23 @@
+# Creating operation functions
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+# Creating a function to show what the use wants to type in
+def what_operation():
+    print("+      for addition")
+    print("-      for subtraction")
+    print("*      for multiplication")
+    print("/      for division")
